9b267feac4c70a28e4e1991bad6af81cb5f14d10
Pixelverk <Pixelverk@gmail.com>
2024-02-15 20:28:56 +0100

    server start time is now in db, to be used on leaderboard

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/game/', leaderboard);
 // use static files
 app.use(express.static('./public'))
 
-// temporary storage
+// "database"
 const db = {
   startTime: '',
   players: [],
@@ -63,3 +63,16 @@ io.on("connection", (socket) => {
   });
 
 });
+
+// get server start time
+function fancyDate(){
+  let date_ob = new Date();
+  let date = ("0" + date_ob.getDate()).slice(-2);
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+  let year = date_ob.getFullYear();
+  let hours = date_ob.getHours();
+  let minutes = date_ob.getMinutes();
+  return (year + "-" + month + "-" + date + " " + hours + ":" + minutes);
+}
+// store server start time in DB
+db.startTime = fancyDate();
